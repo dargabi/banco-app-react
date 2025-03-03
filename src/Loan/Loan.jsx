e3ab09f951b3ce15fcb03f93095222faa5532644
Gabriel Urbano <g.urbanogrc@gmail.com>
2025-03-03 09:36:53 +0100

    feat(loan): Refactor loan component and improve form handling

--- src/Loan/Loan.jsx
+++ src/Loan/Loan.jsx
@@ -1,38 +1,34 @@
-import "../App.css"
-import {useRef } from "react";
+import "../App.css";
+import { useRef } from "react";
 
-function Loan({movements}){
-  const amountRef = useRef()
-  const limit = 200
+function Loan({ movements }) {
+  const amountRef = useRef();
+  const limit = 200;
 
+  const loan = (e) => {
+    e.preventDefault();
+    const amountLoan = Number(amountRef.current.value);
+    const originBalance = movements.reduce((total, movement) => total + movement, 0);
+    const limitLoan = (limit / 100) * originBalance;
 
-  const loan = function(e){
-    e.preventDefault()
-    const amountLoan = Number(amountRef.current.value)
-    const originBalance = movements.reduce((total, movement) => total + movement, 0)
-    const limitLoan = (limit/100) * originBalance
-    if(amountLoan > 0 && amountLoan <= limitLoan  ){
-      movements.push(amountLoan)
-      console.log(movements)
+    if (amountLoan > 0 && amountLoan <= limitLoan) {
+      movements.push(amountLoan);
+      console.log(movements);
+    } else {
+      console.log("No cumple requisitos");
     }
-    else{
-      console.log("No cumple requisitos")
-    }
+  };
 
-  }
-
-
-    return(
-        <div className="operation operation--loan">
-        <h2>Request loan</h2>
-        <form className="form form--loan">
-          <input type="number" className="form__input form__input--loan-amount" ref={amountRef}/>
-          <button className="form__btn form__btn--loan" onClick={loan}>&rarr;</button>
-          <label className="form__label form__label--loan">Amount</label>
-        </form>
-      </div>
-
-    )
+  return (
+    <div className="operation operation--loan">
+      <h2>Request loan</h2>
+      <form className="form form--loan" onSubmit={loan}>
+        <input type="number" className="form__input form__input--loan-amount" ref={amountRef} />
+        <button type="submit" className="form__btn form__btn--loan">&rarr;</button>
+        <label className="form__label form__label--loan">Amount</label>
+      </form>
+    </div>
+  );
 }
 
-export default Loan
+export default Loan;
